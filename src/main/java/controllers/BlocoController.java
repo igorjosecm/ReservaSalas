c0b331944a436d7f6dc36a1ccc774aecfa30f6b2
GabeZape <gabrielzapellaro@gmail.com>
2025-05-07 14:20:47 -0300

    Construção controllers

--- src/main/java/controllers/BlocoController.java
+++ src/main/java/controllers/BlocoController.java
@@ -1,4 +1,105 @@
 package controllers;
 
+import classes.Bloco;
+import classes.CompositeKey;
+import dao.BlocoDAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
 public class BlocoController {
+    private final BlocoDAO blocoDAO;
+
+    public BlocoController(Connection connection) {
+        this.blocoDAO = new BlocoDAO(connection);
+    }
+
+    public void createBloco() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Cadastro de Bloco");
+        System.out.print("Código do bloco: ");
+        String codBloco = input.nextLine();
+        System.out.print("Nome do bloco: ");
+        String nomeBloco = input.nextLine();
+        System.out.print("Número de andares: ");
+        int numAndares = Integer.parseInt(input.nextLine());
+
+        Bloco bloco = new Bloco();
+        bloco.setCodigoBloco(codBloco);
+        bloco.setNomeBloco(nomeBloco);
+        bloco.setNumAndares(numAndares);
+
+        blocoDAO.create(bloco);
+        System.out.println("\nBloco criado com sucesso!");
+    }
+
+    public void updateBloco() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Atualização de bloco");
+        System.out.print("Código do bloco: ");
+        String codBloco = input.nextLine();
+        System.out.print("Nome do bloco: ");
+        String nomeBloco = input.nextLine();
+        System.out.print("Número de andares: ");
+        int numAndares = Integer.parseInt(input.nextLine());
+
+        Bloco bloco = new Bloco();
+        bloco.setCodigoBloco(codBloco);
+        bloco.setNomeBloco(nomeBloco);
+        bloco.setNumAndares(numAndares);
+
+        blocoDAO.update(bloco);
+        System.out.println("\nBloco atualizado com sucesso!");
+    }
+
+    public void deleteBloco() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Remoção de bloco");
+        System.out.print("Código do bloco: ");
+        String codBloco = input.next();
+
+        CompositeKey key = new CompositeKey();
+        key.addKey("codigo_bloco", codBloco);
+
+        blocoDAO.delete(key);
+        System.out.println("\nBloco removido com sucesso!");
+    }
+
+    public void findBlocoById() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Busca de bloco por ID");
+        System.out.print("Código do bloco: ");
+        String codBloco = input.next();
+
+        CompositeKey key = new CompositeKey();
+        key.addKey("codigo_bloco", codBloco);
+
+        Bloco bloco = blocoDAO.findById(key);
+        if (bloco != null) {
+            System.out.println("\nBloco encontrado:");
+            System.out.println("Nome: " + bloco.getNomeBloco());
+            System.out.println("Número de andares: " + bloco.getNumAndares());
+        } else {
+            System.out.println("\nBloco não encontrado.");
+        }
+    }
+
+    public void findAllBlocos() throws SQLException {
+        System.out.println("\nListando todas os blocos:");
+        List<Bloco> blocos = blocoDAO.findAll();
+
+        if (blocos.isEmpty()) {
+            System.out.println("\nNenhum bloco encontrado.");
+            return;
+        }
+
+        for (Bloco bloco : blocos) {
+            System.out.println("\n----------------------------------");
+            System.out.println("Código do Bloco: " + bloco.getCodigoBloco());
+            System.out.println("Nome: " + bloco.getNomeBloco());
+            System.out.println("Número de andares: " + bloco.getNumAndares());
+        }
+    }
 }
--- src/main/java/controllers/MateriaController.java
+++ src/main/java/controllers/MateriaController.java
@@ -1,4 +1,105 @@
 package controllers;
 
+import classes.CompositeKey;
+import classes.Materia;
+import dao.MateriaDAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
 public class MateriaController {
+    private final MateriaDAO materiaDAO;
+
+    public MateriaController(Connection connection) {
+        this.materiaDAO = new MateriaDAO(connection);
+    }
+
+    public void createMateria() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Cadastro de Materia");
+        System.out.print("Código da materia: ");
+        String codMateria = input.nextLine();
+        System.out.print("Nome da materia: ");
+        String nomeMateria = input.nextLine();
+        System.out.print("Carga horária: ");
+        int cargaHoraria = Integer.parseInt(input.nextLine());
+
+        Materia materia = new Materia();
+        materia.setCodigoMateria(codMateria);
+        materia.setNomeMateria(nomeMateria);
+        materia.setCargaHoraria(cargaHoraria);
+
+        materiaDAO.create(materia);
+        System.out.println("\nMateria criada com sucesso!");
+    }
+
+    public void updateMateria() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Atualização de materia");
+        System.out.print("Código da materia: ");
+        String codMateria = input.nextLine();
+        System.out.print("Novo nome da materia: ");
+        String nomeMateria = input.nextLine();
+        System.out.print("Novo carga horaria: ");
+        int cargaHoraria = Integer.parseInt(input.nextLine());
+
+        Materia materia = new Materia();
+        materia.setCodigoMateria(codMateria);
+        materia.setNomeMateria(nomeMateria);
+        materia.setCargaHoraria(cargaHoraria);
+
+        materiaDAO.update(materia);
+        System.out.println("\nMateria atualizada com sucesso!");
+    }
+
+    public void deleteMateria() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Remoção de materia");
+        System.out.print("Código da materia: ");
+        String codMateria = input.next();
+
+        CompositeKey key = new CompositeKey();
+        key.addKey("codigo_materia", codMateria);
+
+        materiaDAO.delete(key);
+        System.out.println("\nMateria removida com sucesso!");
+    }
+
+    public void findMateriaById() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n- Busca de materia por ID");
+        System.out.print("Código da materia: ");
+        String codMateria = input.next();
+
+        CompositeKey key = new CompositeKey();
+        key.addKey("codigo_materia", codMateria);
+
+        Materia materia = materiaDAO.findById(key);
+        if (materia != null) {
+            System.out.println("\nMateria encontrada:");
+            System.out.println("Nome: " + materia.getNomeMateria());
+            System.out.println("Carga horária: " + materia.getCargaHoraria());
+        } else {
+            System.out.println("\nMateria não encontrada.");
+        }
+    }
+
+    public void findAllMaterias() throws SQLException {
+        System.out.println("\nListando todas as materias:");
+        List<Materia> materias = materiaDAO.findAll();
+
+        if (materias.isEmpty()) {
+            System.out.println("\nNenhuma materia encontrada.");
+            return;
+        }
+
+        for (Materia materia : materias) {
+            System.out.println("\n----------------------------------");
+            System.out.println("Código da materia: " + materia.getCodigoMateria());
+            System.out.println("Nome: " + materia.getNomeMateria());
+            System.out.println("Carga horária: " + materia.getCargaHoraria());
+        }
+    }
 }
