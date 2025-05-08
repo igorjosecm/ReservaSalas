185df41c4b8e8e1777673ba778bc7e70d850e793
Igor <igorjosedacosta@hotmail.com>
2025-05-07 21:54:17 -0300

    helper methods de data e hora

--- src/main/java/helpers/Helpers.java
+++ src/main/java/helpers/Helpers.java
@@ -1,5 +1,9 @@
 package helpers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Helpers {
@@ -20,6 +24,30 @@ public class Helpers {
                 return Integer.parseInt(input.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("Valor inválido. Por favor informe um número inteiro.");
+            }
+        }
+    }
+
+    public static LocalDate getLocalDateInput(Scanner input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        while (true) {
+            try {
+                String localDateInput = input.nextLine();
+                return LocalDate.parse(localDateInput, formatter);
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de data inválido. Use dd/mm/aaaa.");
+            }
+        }
+    }
+
+    public static LocalTime getLocalTimeInput(Scanner input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm");
+        while (true) {
+            try {
+                String localDateInput = input.nextLine();
+                return LocalTime.parse(localDateInput, formatter);
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato de hora inválido. Use hh:mm.");
             }
         }
     }
